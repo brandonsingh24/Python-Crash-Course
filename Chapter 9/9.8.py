a62a8b1c319f9294c05e219aa5d92711af768014
brandonsingh24 <brandonproductions24@gmail.com>
2024-08-03 12:51:47 -0400

    Create 9.8.py

---
+++ Chapter 9/9.8.py
@@ -0,0 +1,16 @@
+##Admin
+
+class Privileges:
+    
+    def __init__(self, privileges):
+        self.privileges = privileges
+
+    def show_privileges(self):
+        print("Privileges:")
+        for privilege in self.privileges:
+            print(f"- {privilege}")
+
+Userprivileges = Privileges(['can add post', 'can delete post', 'can ban user'])
+
+
+Userprivileges.show_privileges()
